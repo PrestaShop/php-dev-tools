ce42e6618b0eb812736658b0a5c289d7302ff644
Thomas Nabord <thomas.nabord@prestashop.com>
2019-11-28 17:53:02 +0000

    Move php-parser from phpstan at the first position in the autoloaders list

--- templates/phpstan/bootstrap.php
+++ templates/phpstan/bootstrap.php
@@ -21,22 +21,22 @@ require_once $rootDir . '/config/bootstrap.php';
 
 // Make sure loader php-parser is coming from php stan composer
 
-// 1- Use with Docker container
+// 1- Use module vendors
+$loader = new \Composer\Autoload\ClassLoader();
+$loader->setPsr4('PhpParser\\', ['vendor/nikic/php-parser/lib/PhpParser']);
+$loader->register(true);
+// 2- Use with Docker container
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->setPsr4('PhpParser\\', ['/composer/vendor/nikic/php-parser/lib/PhpParser']);
 $loader->register(true);
-// 2- Use with PHPStan phar
+// 3- Use with PHPStan phar
 $loader = new \Composer\Autoload\ClassLoader();
 // Contains the vendor in phar, like "phar://phpstan.phar/vendor"
 $loader->setPsr4('PhpParser\\', ['phar://' . dirname($_SERVER['PATH_TRANSLATED']) . '/../phpstan/phpstan-shim/phpstan.phar/vendor/nikic/php-parser/lib/PhpParser/']);
 $loader->register(true);
-// 3- Use phpstan phar with sym link
+// 4- Use phpstan phar with sym link
 $loader = new \Composer\Autoload\ClassLoader();
 $loader->setPsr4('PhpParser\\', ['phar://' . realpath($_SERVER['PATH_TRANSLATED']) . '/vendor/nikic/php-parser/lib/PhpParser/']);
-$loader->register(true);
-// 4 - Use module vendors
-$loader = new \Composer\Autoload\ClassLoader();
-$loader->setPsr4('PhpParser\\', ['vendor/nikic/php-parser/lib/PhpParser']);
 $loader->register(true);
 
 // We must declare these constant in this boostrap script.
