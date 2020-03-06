b821b27672956e2bc3632b89433fe7ce7ec712fc
Alexis Haumaitre <haumalex@gmail.com>
2020-03-06 14:06:49 +0100

    define constant with proper type

--- phpstan/bootstrap.php
+++ phpstan/bootstrap.php
@@ -44,44 +44,54 @@ $loader->register(true);
 // Ignoring the error partern with this value will throw another error if not found
 // during the checks.
 $constantsToDefine = [
-  '_DB_SERVER_',
-  '_DB_NAME_',
-  '_DB_USER_',
-  '_DB_PASSWD_',
-  '_MYSQL_ENGINE_',
-  '_COOKIE_KEY_',
-  '_COOKIE_IV_',
-  '_PS_VERSION_',
-  '_DB_PREFIX_',
-  '_PS_SSL_PORT_',
-  '_THEME_NAME_',
-  '_THEME_COL_DIR_',
-  '_PARENT_THEME_NAME_',
-  '__PS_BASE_URI__',
-  '_PS_PRICE_DISPLAY_PRECISION_',
-  '_PS_PRICE_COMPUTE_PRECISION_',
-  '_PS_OS_CHEQUE_',
-  '_PS_OS_PAYMENT_',
-  '_PS_OS_PREPARATION_',
-  '_PS_OS_SHIPPING_',
-  '_PS_OS_DELIVERED_',
-  '_PS_OS_CANCELED_',
-  '_PS_OS_REFUND_',
-  '_PS_OS_ERROR_',
-  '_PS_OS_OUTOFSTOCK_',
-  '_PS_OS_OUTOFSTOCK_PAID_',
-  '_PS_OS_OUTOFSTOCK_UNPAID_',
-  '_PS_OS_BANKWIRE_',
-  '_PS_OS_PAYPAL_',
-  '_PS_OS_WS_PAYMENT_',
-  '_PS_OS_COD_VALIDATION_',
-  '_PS_THEME_DIR_',
-  '_PS_BASE_URL_',
-  '_MODULE_DIR_'
+  '_DB_SERVER_' => 'string',
+  '_DB_NAME_' => 'string',
+  '_DB_USER_' => 'string',
+  '_DB_PASSWD_' => 'string',
+  '_MYSQL_ENGINE_' => 'string',
+  '_COOKIE_KEY_' => 'string',
+  '_COOKIE_IV_' => 'string',
+  '_DB_PREFIX_' => 'string',
+  '_PS_SSL_PORT_' => 'int',
+  '_THEME_NAME_' => 'string',
+  '_THEME_COL_DIR_' => 'string',
+  '_PARENT_THEME_NAME_' => 'string',
+  '__PS_BASE_URI__' => 'string',
+  '_PS_PRICE_DISPLAY_PRECISION_' => 'int',
+  '_PS_PRICE_COMPUTE_PRECISION_' => 'string',
+  '_PS_OS_CHEQUE_' => 'int',
+  '_PS_OS_PAYMENT_' => 'int',
+  '_PS_OS_PREPARATION_' => 'int',
+  '_PS_OS_SHIPPING_' => 'int',
+  '_PS_OS_DELIVERED_' => 'int',
+  '_PS_OS_CANCELED_' => 'int',
+  '_PS_OS_REFUND_' => 'int',
+  '_PS_OS_ERROR_' => 'int',
+  '_PS_OS_OUTOFSTOCK_' => 'int',
+  '_PS_OS_OUTOFSTOCK_PAID_' => 'int',
+  '_PS_OS_OUTOFSTOCK_UNPAID_' => 'int',
+  '_PS_OS_BANKWIRE_' => 'int',
+  '_PS_OS_PAYPAL_' => 'int',
+  '_PS_OS_WS_PAYMENT_' => 'int',
+  '_PS_OS_COD_VALIDATION_' => 'int',
+  '_PS_THEME_DIR_' => 'string',
+  '_PS_BASE_URL_' => 'string',
+  '_MODULE_DIR_' => 'string'
 ];
 
-foreach ($constantsToDefine as $constant) {
-    if (!defined($constant)) {
-        define($constant, 'DUMMY_VALUE');
+foreach ($constantsToDefine as $key => $value) {
+    if (!defined($value)) {
+        switch ($value) {
+            case 'string':
+                define($key, 'DUMMY_VALUE');
+            break;
+            case 'int':
+                define($key, 1);
+            break;
+            default:
+                define($key, 'DUMMY_VALUE');
+            break;
+        }
     }
 }
+
